62da7286e1c15a4e6d961927939486a1dc1d2288
Butnamfa <butnamfa.ram@gmail.com>
2025-11-15 14:34:42 +0700

    add paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
     <h1 className="text-3xl font-bold underline text-purple-600 animate-bounce">
       Hello world!
     </h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut iusto consequuntur nemo pariatur esse laudantium nisi hic quos beatae quidem at, assumenda fugit doloribus. Nihil voluptates nostrum atque a labore.</p>
     </>
   )
 }
